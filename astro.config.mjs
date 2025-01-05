d1808db7ebec2233862c006d3af28f37e61dc9a2
Emi Nihonyanagi <emipuu33@gmail.com>
2025-01-06 00:47:17 +0900

    js、scssをsrcに設置

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,33 @@ import compress from 'vite-plugin-compression';
 export default defineConfig({
   site: 'https://portfolio.twowillowsdesign.com/',
   trailingSlash: "always",
+  build: {
+    assets: "assets",
+    inlineStylesheets: `never`,
+  },
   vite: {
     plugins: [compress()],
     resolve: {
       alias: {
         "@assets": "/src/assets",
         jquery: "jquery",
+      },
+    },
+    build: {
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/js/main.js`,
+          assetFileNames: (assetInfo) => {
+            const extType = assetInfo.name.split(".")[1];
+
+            if (extType === "css") {
+              return "assets/css/main.css";
+            }
+
+            return "assets/[name].[ext]";
+          },
+        },
       },
     },
   },
